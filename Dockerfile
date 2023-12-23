3a6f187671000fa79388ecc4b42f1ce6b6ada2df
TheRealPad <pierre-alexandre.delgado-arevalo@epitech.eu>
2023-12-22 19:43:28 -0800

    feat(client): init react + vite application

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM node
+WORKDIR /app
+COPY . .
+RUN yarn
+EXPOSE 3000
+CMD ["yarn", "dev"]
---
+++ config/client/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    open: false,
+    port: 3000,
+    host: true,
+    proxy: {
+      "/api": {
+        target: process.env.VITE_API_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+  envDir: "../../.env",
+});
