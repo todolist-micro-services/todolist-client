c0e024670e15d07dc02bb549a0d0db352b5c38c7
TheRealPad <pierre-alexandre.delgado-arevalo@epitech.eu>
2024-01-06 16:43:37 -0800

    feat(submodule): add client-core as gitsubmodule

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -2,9 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@pages/home";
 import "./index.scss";
+import { Provider } from "react-redux";
+import { store } from "../todolist-client-core/src/utils/store.ts";
+import reportWebVitals from "./reportWebVitals.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
+
+reportWebVitals();
--- src/client/pages/home/index.tsx
+++ src/client/pages/home/index.tsx
@@ -1,29 +1,46 @@
 import { useState } from "react";
-import viteLogo from "/vite.svg";
 import styles from "./index.module.scss";
+import { useCountUpdate } from "../../../todolist-client-core/src/viewModels/counter";
+import { useAppSelector } from "../../../todolist-client-core/src/utils/hook.ts";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [incrementAmount, setIncrementAmount] = useState("2");
+
+  const incrementValue = Number(incrementAmount) || 0;
+  const {
+    updateCount,
+    count,
+    isRequestFailure,
+    isRequestPending,
+    isRequestSuccess,
+  } = useCountUpdate();
+  const errorMessage = useAppSelector((state) => state.error);
 
   return (
-    <div className={styles.root}>
+    <div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className={styles.logo} alt="Vite logo" />
-        </a>
+        {errorMessage["errorMessage"] && <p>{errorMessage["errorMessage"]}</p>}
+        {isRequestPending && <p>pending</p>}
+        {isRequestFailure && <p>failure</p>}
+        {isRequestSuccess && <p>success</p>}
       </div>
-      <h1>Vite + React</h1>
-      <div className={styles.card}>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className={styles.row}>
+        <span className={styles.value}>{count}</span>
+      </div>
+      <div className={styles.row}>
+        <input
+          className={styles.textbox}
+          aria-label="Set increment amount"
+          value={incrementAmount}
+          onChange={(e) => setIncrementAmount(e.target.value)}
+        />
+        <button
+          className={styles.asyncButton}
+          onClick={() => updateCount(incrementValue)}
+        >
+          Add Async 1
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className={styles.readTheDocs}>
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   );
 }
